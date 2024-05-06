f68bf34bf542b00ad98a9925854aabcb4ba06e77
debilanakoncar <168759994+debilanakoncar@users.noreply.github.com>
2024-05-06 22:09:06 +0200

    formatiranje broja

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 const targetDate = new Date('2024-06-21T00:00:00').getTime() / 1000;
-
 
 const timerInterval = setInterval(updateTimer, 1000);
 
@@ -8,13 +6,17 @@ function updateTimer() {
   const nowSeconds = Math.floor(Date.now() / 1000);
   const distance = targetDate - nowSeconds;
 
+  const formattedDistance = numberWithSpaces(distance);
 
   const timerElement = document.getElementById('timer');
-  timerElement.innerHTML = `${distance} sekundi`;
-
+  timerElement.innerHTML = `${formattedDistance} sekundi`;
 
   if (distance <= 0) {
     clearInterval(timerInterval);
     timerElement.innerHTML = 'SJAŠI BILJANAAAAAAAAAAAAAAA!!!!!!';
   }
+}
+
+function numberWithSpaces(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
