51d83039fbcc33c919a0209a4bf1e408fde60dcd
debilanakoncar <168759994+debilanakoncar@users.noreply.github.com>
2024-09-09 22:13:54 +0200

    Novi datum

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date('2024-06-21T00:00:00').getTime() / 1000;
+const targetDate = new Date('2024-05-23T00:00:00').getTime() / 1000;
 
 const timerInterval = setInterval(updateTimer, 1000);
 
